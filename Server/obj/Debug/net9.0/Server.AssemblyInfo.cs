57e7cee9f00b21e616b05ba0c9cc1c75c75c959d
Sophia S <sophia.saks@live.se>
2024-12-19 20:20:52 +0100

    wip

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a19ebafe1074dbbfd5cfe780044fa56e399349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481a2ec3fd7aca9481bfac272d757b0bc39f7894")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
